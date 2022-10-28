44c333ded678876f647721d13b532fe70f302ca2
Zack Plauche <zackplauche@gmail.com>
2022-10-28 16:39:22 +0300

    Update instructions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>Home</h1>
-    <p>Check your developer console and see the global <i>FetchClient</i> object with a "baseURL" that came from an environment variable in Vite.</p>
-    <p>This is a Vite + Vue project, but <b>I'm trying to build this same functionality in Nuxt 3.</b></p>
+    <h2>To see what I'm trying to accomplish in Nuxt 3...</h2>
+    <p>1. Open your developer console and see the global <i>FetchClient</i> object with a "baseURL" key that came from an environment variable in Vite.</p>
+    <p>2.<b>That ^ baseURL environment variable piece the main thing I'm trying to replicate in Nuxt.</b></p>
+    <p>3. Now go to the <router-link to="/login"><button>Login</button></router-link> page and see that this global FetchClient still has that baseURL AND I was able to have auth headers that persist across pages.</p>
   </div>
 </template>
 
